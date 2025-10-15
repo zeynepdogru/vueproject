75ecb1354c8ac80fdaff1684232e9d24cba475b0
zeynepdogru <zynp11dgru@gmail.com>
2025-10-15 18:45:54 +0300

    feat: login screen designed using slots, emits, and props in component structure

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <formOne v-bind:title="title" v-on:submitForm="submitForm">
+    <template #header>
+      <h2>Vue Form Kullanımı</h2>
+    </template>
+  </formOne>
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+
+export default {
+  components: {
+    formOne: Form,
+  },
+
+  data() {
+    return {
+      //props ile başlık gönderme
+      title: "Giriş Formu",
+    };
+  },
+  methods: {
+    submitForm(formData) {
+      alert(`E-posta: ${formData.email} Şifre: ${formData.password}`);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Form.vue
@@ -0,0 +1,118 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div class="form">
+    <slot name="header"></slot>
+    <div class="form-container">
+      <p>Giriş yapabilmek için lütfen formu doldurun.</p>
+    </div>
+    <div class="form-input">
+      <label for="email">E-Posta</label>
+      <input
+        v-model="email"
+        type="email"
+        name="email"
+        placeholder="E-posta adresinizi giriniz"
+      />
+      <div class="form-input-description">Bu e-posta içeriği: {{ email }}</div>
+
+      <label for="password">Şifre</label>
+      <input
+        v-model.lazy="password"
+        type="password"
+        name="password"
+        placeholder="Şifrenizi giriniz"
+      />
+    </div>
+    <div class="form-input-description">Bu şifre içeriği: {{ password }}</div>
+
+    <div class="form-button">
+      <button v-on:click="handleClick">Giriş Yap</button>
+    </div>
+
+    <div class="footer">
+      <p>© 2025 Vue Blog</p>
+      <p>Tüm Hakları Saklıdır</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleClick() {
+      this.$emit("submitForm", { email: this.email, password: this.password });
+    },
+  },
+};
+</script>
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 2rem;
+  color: #35495e;
+}
+
+:slotted(h2) {
+  text-align: center;
+  margin-bottom: 1rem;
+  color: #42b983;
+}
+
+.form {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 2px 2px 8px rgba(50, 174, 108, 0.1);
+}
+.form-container {
+  text-align: center;
+  margin-bottom: 1rem;
+}
+
+.form-input label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+.form-input input {
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 0.5rem;
+}
+.form-input-description {
+  font-style: italic;
+  color: #666;
+  margin-bottom: 0.5rem;
+}
+.form-button button {
+  background-color: #42b983;
+  color: white;
+  border: none;
+  padding: 0.75rem 1.5rem;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 1rem;
+  margin: 2px auto;
+  display: block;
+  width: 100%;
+}
+.footer {
+  text-align: center;
+  margin-top: 1rem;
+  color: #888;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
+</style>
